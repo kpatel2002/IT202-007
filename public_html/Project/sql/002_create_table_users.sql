56fbc51b7aab899b305d30e8dad50f0f7f25cc47
kpatel2002 <90267637+kpatel2002@users.noreply.github.com>
2021-12-09 16:12:20 -0500

    'maybe

--- public_html/Project/sql/002_create_table_users.sql
+++ public_html/Project/sql/002_create_table_users.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS  `Roles`
 (
     `id`         int auto_increment not null,
-    `name`      varchar(20)       not null unique,
+    `name`      varchar(100)       not null unique,
     `description` varchar(100) default '',
     `is_active`  TINYINT(1) default 1,
     `created`    timestamp default current_timestamp,
